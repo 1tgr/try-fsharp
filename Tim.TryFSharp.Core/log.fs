ce9c6b14f4d5a1eec66d54186b77eb13fbe72012
Tim Robinson <tim.g.robinson@gmail.com>
2011-02-13 19:30:08 +0000

    Try the new auto deploy thing

--- Tim.TryFSharp.Core/log.fs
+++ Tim.TryFSharp.Core/log.fs
@@ -4,4 +4,4 @@ open System
 
 module Log =
     let info format =
-        Printf.ksprintf (fun s -> Console.WriteLine("[{0:o}] {1}", DateTime.Now, s)) format
+        Printf.ksprintf (fun s -> Console.WriteLine("[{0:o} {1}] {2}", DateTime.Now, AppDomain.CurrentDomain.FriendlyName, s)) format
