957926b749b50f9529550d4fcabc4d1c5d0cbc9a
Tim Robinson <tim.g.robinson@gmail.com>
2011-02-20 17:38:30 +0000

    Defer subscription until it's needed

--- couchdb/_attachments/index.js
+++ couchdb/_attachments/index.js
@@ -1,8 +1,23 @@
 function init() {
-    var sessionId = $.couch.newUUID();
+    var sessionId;
     var db = $.couch.db("tryfs");
 
+    function onChange(resp) {
+      $.each(resp.results, function() {
+        var message = this.doc.message ? this.doc.message : "\n";
+        console.commandResult(message, "jquery-console-message-success");
+      });
+    }
+
+    function subscribe() {
+        if (!sessionId) {
+            sessionId  = $.couch.newUUID();
+            db.changes(null, { filter: "app/session", include_docs: true, sessionId: sessionId }).onChange(onChange);
+        }
+    }
+
     function commandHandle(code) {
+        subscribe();
         db.saveDoc({ messageType: "in", message: code, sessionId: sessionId });
         return true;
     }
@@ -13,23 +28,12 @@ function init() {
     });
 
     function onSend() {
+        subscribe();
         var code = $("#code").val() + ";;";
         console.commandResult(code, "jquery-console-message-success");
         db.saveDoc({ messageType: "in", message: code, sessionId: sessionId });
     }
 
-    function onChange(resp) {
-      $.each(resp.results, function() {
-        var message = this.doc.message ? this.doc.message : "\n";
-        console.commandResult(message, "jquery-console-message-success");
-      });
-    }
-
-    function subscribe() {
-      db.changes(null, { filter: "app/session", include_docs: true, sessionId: sessionId }).onChange(onChange);
-    } 
-
-    setTimeout(subscribe, 500);
     $("#send").click(onSend);
 }
 
