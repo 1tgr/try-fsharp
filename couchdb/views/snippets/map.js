b3ee486f6be626060e39ec492d4f89873809f5fe
Tim Robinson <tim.g.robinson@gmail.com>
2011-02-27 18:19:20 +0000

    View for snippets

---
+++ couchdb/views/snippets/map.js
@@ -0,0 +1,13 @@
+function(doc) {
+  if (doc.type == "snippet") {
+    var doc2 = {};
+    for (name in doc) {
+      if (doc.hasOwnProperty(name)) {
+        doc2[name] = doc[name];
+      }
+    }
+
+    delete doc2.code;
+    emit(null, doc2);
+  }
+}
