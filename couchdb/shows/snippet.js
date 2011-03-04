3acf3a89f37c04ef221327093cb3e0f9d755b806
Tim Robinson <tim.g.robinson@gmail.com>
2011-03-04 21:41:38 +0000

    Added link back to fssnip.net

--- couchdb/shows/snippet.js
+++ couchdb/shows/snippet.js
@@ -1,5 +1,13 @@
 function(doc, req) {  
     var Mustache = require("lib/mustache");
     doc.head = { title: doc.title };
-    return Mustache.to_html(this.templates.snippet, doc, this.templates.partials);
+    doc.original = { link: doc.link };
+
+    var partials = this.templates.partials;
+    partials.original =
+        doc.link
+        ? partials.snippet_original
+        : partials.snippet_nooriginal;
+
+    return Mustache.to_html(this.templates.snippet, doc, partials);
 }
