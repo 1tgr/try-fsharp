b4adc8c39432df71ccc234e9c653faa40e8bb465
Tim Robinson <tim.g.robinson@gmail.com>
2011-03-20 22:40:32 +0000

    Decode F# names

--- Tim.FSIndex.Console/main.fs
+++ Tim.FSIndex.Console/main.fs
@@ -14,21 +14,30 @@ type AssemblyName =
         PublicKeyToken : string
     }
 
+type TypeName =
+    {
+        Assembly : AssemblyName option
+        Namespace : string option
+        Name : string
+    }
+
 type Method =
     {
         Name : string
+        Prototype : TypeName array
     }
 
 type Property =
     {
         Name : string
+        Type : TypeName
     }
 
 type Type =
     {
         [<JsonName("_rev")>] Rev : string option
         Type : string
-        AssemblyName : AssemblyName
+        Assembly : AssemblyName
         Namespace : string
         Name : string
         Methods : Method array
@@ -59,49 +68,163 @@ module Program =
             PublicKeyToken = hex assembly.Name.PublicKeyToken
         }
 
-    let typeId (assemblyName : AssemblyName) (nspace : string) (name : string) : string =
-        [| assemblyName.Name; assemblyName.Version; assemblyName.PublicKeyToken; nspace; name |]
+    let sysAssemblyName (assembly : System.Reflection.Assembly) : AssemblyName =
+        let assemblyName = assembly.GetName()
+        
+        {
+            Name = assemblyName.Name
+            Version = string assemblyName.Version
+            PublicKeyToken = hex (assemblyName.GetPublicKeyToken())
+        }
+
+    let typeId (assembly : AssemblyName) (nspace : string) (name : string) : string =
+        [| assembly.Name; assembly.Version; assembly.PublicKeyToken; nspace; name |]
         |> String.concat ","
         |> Encoding.UTF8.GetBytes
         |> md5
 
-    let types (assembly : AssemblyDefinition) : Type list =
-        let assemblyName = assemblyName assembly
+    let resolve (typ : TypeReference) : TypeDefinition =
+        match typ.Resolve() with
+        | null -> failwithf "Unable to resolve %s %s" (typ.GetType().Name) typ.FullName
+        | t -> t
 
+    let decodeName<'a when 'a :> ICustomAttributeProvider and 'a :> IMemberDefinition> (mem : 'a) : string =
+        let name =
+            match mem.CustomAttributes |> Seq.tryFind (fun a -> a.AttributeType.FullName = "Microsoft.FSharp.Core.CompilationRepresentationAttribute") with
+            | Some cra ->
+                let flags : CompilationRepresentationFlags = unbox cra.ConstructorArguments.[0].Value
+                match int (flags &&& CompilationRepresentationFlags.ModuleSuffix) with
+                | 0 -> None
+                | _ -> Some (mem.Name.Substring(0, mem.Name.Length - "Module".Length))
+            
+            | None ->
+                None
+
+        let name =
+            match name, mem.CustomAttributes |> Seq.tryFind (fun a -> a.AttributeType.FullName = "Microsoft.FSharp.Core.CompilationSourceNameAttribute") with
+            | None, Some csna ->
+                Some (unbox csna.ConstructorArguments.[0].Value)
+
+            | opt, _ ->
+                opt
+
+        defaultArg name mem.Name
+
+    let rec typeRefName (g : int) (typeRef : TypeReference) : int * TypeName =
+        match typeRef with
+        | :? ArrayType as at ->
+            let g, elementType = typeRefName g at.ElementType
+            g, {
+                TypeName.Assembly = None
+                Namespace = None
+                Name = elementType.Name + " array"
+            }
+
+        | :? ByReferenceType as brt ->
+            let g, elementType = typeRefName g brt.ElementType
+            g, {
+                TypeName.Assembly = None
+                Namespace = None
+                Name = sprintf "byref<%s>" elementType.Name
+            }
+
+        | :? GenericParameter as gp ->
+            g + 1, {
+                TypeName.Assembly = None
+                Namespace = None
+                Name = sprintf "'%c" (char ((int 'a') + g))
+            }
+
+        | _ ->
+            let typ = resolve typeRef
+            g, {
+                TypeName.Assembly = Some (assemblyName typ.Module.Assembly)
+                Namespace = Some typ.Namespace
+                Name = decodeName typ
+            }
+
+    let prototype (typeRefs : TypeReference array) : TypeName array =
+        let _, typeNames =
+            let folder (g, typeNames) typeRef =
+                let g, typeName = typeRefName g typeRef
+                g, typeName :: typeNames
+
+            Array.fold folder (0, []) typeRefs
+
+        typeNames
+        |> List.rev
+        |> Array.ofList
+
+    let methd (methd : MethodDefinition) : Method =
+        let prototype =
+            prototype [|
+                for parm in methd.Parameters do
+                    yield parm.ParameterType
+
+                yield methd.ReturnType
+            |]
+
+        let prototype =
+            if methd.Parameters.Count = 0 then
+                let unit = typeof<unit>
+                let unit : TypeName =
+                    {
+                        Assembly = Some (sysAssemblyName unit.Assembly)
+                        Namespace = Some unit.Namespace
+                        Name = unit.Name
+                    }
+
+                Array.append [| unit |] prototype
+            else
+                prototype
+
+        {
+            Name = decodeName methd
+            Prototype = prototype                
+        }
+
+    let property (property : PropertyDefinition) : Property =
+        {
+            Name = decodeName property
+            Type = snd (typeRefName 0 property.PropertyType)
+        }
+
+    let typ (typ : TypeDefinition) : Type =
+        let methods : Method array =
+            [|
+                for m in typ.Methods do
+                    let ignore =
+                        [|
+                            m.IsRuntimeSpecialName
+                            m.IsAddOn
+                            m.IsRemoveOn
+                            m.IsFire
+                            m.IsPrivate
+                        |]
+
+                    if not (Array.exists id ignore) then
+                        yield methd m
+            |]
+
+        let properties : Property array =
+            [| for p in typ.Properties -> property p |]
+                                
+        {
+            Rev = None
+            Type = "type"
+            Assembly = assemblyName typ.Module.Assembly
+            Namespace = typ.Namespace
+            Name = typ.Name
+            Methods = methods
+            Properties = properties
+        }
+
+    let types (assembly : AssemblyDefinition) : Type list =
         [
             for modul in assembly.Modules do
-                for typ in modul.Types do
-                    if typ.IsPublic then
-                        let methods : Method array =
-                            [|
-                                for methd in typ.Methods do
-                                    let ignore =
-                                        [|
-                                            methd.IsRuntimeSpecialName
-                                            methd.IsGetter
-                                            methd.IsSetter
-                                            methd.IsAddOn
-                                            methd.IsRemoveOn
-                                            methd.IsFire
-                                            methd.IsPrivate
-                                        |]
-
-                                    if not (Array.exists id ignore) then
-                                        yield { Name = methd.Name }
-                            |]
-
-                        let properties : Property array =
-                            [| for property in typ.Properties -> { Name = property.Name } |]
-                                
-                        yield {
-                            Rev = None
-                            Type = "type"
-                            AssemblyName = assemblyName
-                            Namespace = typ.Namespace
-                            Name = typ.Name
-                            Methods = methods
-                            Properties = properties
-                        }
+                for t in modul.Types do
+                    if t.IsPublic then
+                        yield typ t
         ]
 
     let rec changeDocument (baseUri : Uri) (id : string) (fn : 'a option -> 'a) : unit =
@@ -118,11 +241,11 @@ module Program =
                 changeDocument baseUri id fn
 
     let index baseUri (filename : string) =
-        let rec impl garbage typs =
+        let rec impl garbage (typs : Type list) =
             match typs with
             | [] -> garbage
             | typ :: typs ->
-                let id = typeId typ.AssemblyName typ.Namespace typ.Name
+                let id = typeId typ.Assembly typ.Namespace typ.Name
 
                 changeDocument baseUri id <| function
                     | Some t -> { typ with Rev = t.Rev }
