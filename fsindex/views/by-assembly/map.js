af2e1bc37d6b1d01e50932d926e6474929eb1509
Tim Robinson <tim.g.robinson@gmail.com>
2011-04-04 10:05:05 +0100

    Fix by-assembly view

--- fsindex/views/by-assembly/map.js
+++ fsindex/views/by-assembly/map.js
@@ -1,5 +1,5 @@
 function(doc) {
   if (doc.type === "type") {
-    emit(doc.assemblyName, { _rev: doc._rev, namespace: doc.namespace, name: doc.name });
+    emit(doc.assembly, { _rev: doc._rev, namespace: doc.namespace, name: doc.name });
   }
 }
