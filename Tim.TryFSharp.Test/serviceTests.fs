263fe3ad903777fb98cf950837eb57bf94dec915
Tim Robinson <tim.g.robinson@gmail.com>
2012-11-10 21:07:35 +0000

    Tests for fsi.exe execution

--- Tim.TryFSharp.Test/serviceTests.fs
+++ Tim.TryFSharp.Test/serviceTests.fs
@@ -1,9 +1,22 @@
 ﻿namespace Tim.TryFSharp.Test
 
+open System
+open System.IO
+open System.Threading
+open Tim.TryFSharp.Service
 open Xunit
 
 type ServiceTests() =
 
     [<Fact>]
-    let dummy () =
-        Assert.True(false, "This is a dummy test for AppHarbor")
+    let ``Should start and stop fsi`` () =
+        use proc = new FsiProcess({ Name = Path.GetRandomFileName(); InitTexts = [| |]; Arguments = [| |]; Print = Console.WriteLine; Recycle = id })
+        ()
+
+    [<Fact>]
+    let ``Should interact with fsi`` () =
+        let lines = ResizeArray()
+        using (new FsiProcess({ Name = Path.GetRandomFileName(); InitTexts = [| |]; Arguments = [| |]; Print = lines.Add; Recycle = id })) <| fun fsi ->
+            fsi.Process.StandardInput.WriteLine("printfn \"hello world\";;")
+
+        Assert.Contains("hello world", lines)
