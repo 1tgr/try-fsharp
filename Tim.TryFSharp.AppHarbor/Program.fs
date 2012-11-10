69fb06eb9e7d8097632bf6fdcf682979797a5c5c
Tim Robinson <tim.g.robinson@gmail.com>
2012-11-10 21:41:16 +0000

    Background worker EXE

---
+++ Tim.TryFSharp.AppHarbor/Program.fs
@@ -0,0 +1,20 @@
+﻿namespace Tim.TryFSharp.AppHarbor
+
+open System
+open System.Configuration
+open System.Threading
+open Tim.TryFSharp.Core
+open Tim.TryFSharp.Service
+
+module Program =
+
+    [<EntryPoint>]
+    let main _ =
+        let config : ServiceConfig =
+            match ConfigurationManager.AppSettings.["database"] with
+            | null | "" -> failwith "Missing 'database' config setting. Got: %A" [| for k in ConfigurationManager.AppSettings.Keys -> k |]
+            | s -> { BaseUri = Uri(s) }
+
+        use state = ServiceState.Create(config)
+        Thread.Sleep(Timeout.Infinite)
+        0
