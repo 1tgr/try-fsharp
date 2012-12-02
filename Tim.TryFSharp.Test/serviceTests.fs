210810fcf04b05c037b8758bb7eba68637027078
Tim Robinson <tim.g.robinson@gmail.com>
2012-12-02 18:08:16 +0000

    Relax the test: don't rely on fsi being able to understand UTF-8 input

--- Tim.TryFSharp.Test/serviceTests.fs
+++ Tim.TryFSharp.Test/serviceTests.fs
@@ -14,27 +14,27 @@ type ServiceTests() =
         use fsiProc = new FsiProcess({ Name = Path.GetRandomFileName(); InitTexts = [| |]; Arguments = [| |]; Print = Console.WriteLine; Recycle = id }, proc)
         ()
 
-    let interact message =
+    let interact fn substring =
         let lines = ResizeArray()
         use gotMessage = new ManualResetEvent(false)
 
         let print s =
             lines.Add(s)
-            if (s : string).Contains(message) then
+            if (s : string).Contains(substring) then
                 ignore (gotMessage.Set())
 
         let success =
             use proc = FsiProcess.Start()
             use fsiProc = new FsiProcess({ Name = Path.GetRandomFileName(); InitTexts = [| |]; Arguments = [| |]; Print = print; Recycle = id }, proc)
-            fsiProc.StandardInput.WriteLine("printfn \"{0}\";;", message)
-            gotMessage.WaitOne(TimeSpan.FromSeconds(30.0))
+            fn fsiProc
+            gotMessage.WaitOne(TimeSpan.FromSeconds(10.0))
 
-        Assert.True(success, sprintf "Should have '%s' in: %A" message (lines.ToArray()))
+        Assert.True(success, sprintf "Should have '%s' in: %A" substring (lines.ToArray()))
 
     [<Fact>]
     let ``Should interact with fsi`` () =
-        interact "hello world"
+        interact (fun fsiProc -> fprintfn fsiProc.StandardInput "printfn \"hello world\";;") "hello world"
         
     [<Fact>]
     let ``Standard input and output should use UTF-8`` () =
-        interact "“£10”, he said"
+        interact (fun fsiProc -> fprintfn fsiProc.StandardInput "printfn \"\\u201C\\u00A310\\u201D, he said\"") "“£10”, he said"
